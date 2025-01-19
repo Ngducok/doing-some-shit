94d7085f346f86e60e6e70cd7975779d1d037137
Nguyen Minh Duc <127043162+Ngducok@users.noreply.github.com>
2025-01-19 12:07:07 +0700

    Add files via upload

---
+++ unit.lua
@@ -0,0 +1,77 @@
+    local UnitsRequirement - {
+        iceclaw = {
+            TakeDown = 7500,
+            Yellow_Fruit = 30,
+            Blue_Fruit = 13,
+            Red_Fruit = 13,
+            Pink_Fruit = 10,
+            Green_Fruit = 10,
+            Rainbow_Fruit = 3,
+        }
+        Menance = {
+            takedown = 5000,
+            Crystallite = 4,
+            Dark_Crystallite = 4,
+            Blood_Crystallite = 2,
+            Yellow_Fruit = 4,
+            Blue_Fruit = 1,
+            Red_Fruit = 1,
+            Pink_Fruit = 1,
+            Rainbow_Fruit = 1,
+        }
+
+        Guts = {
+            takedown = 7500,
+            Egg_of_Sacrifice = 50,
+            Yellow_Fruit = 12,
+            Blue_Fruit = 3  ,
+            Red_Fruit = 5,
+            Pink_Fruit = 4,
+            Green_Fruit = 4,
+            Rainbow_Fruit = 1,
+        }
+
+        Honey = {
+            takedown = 100,
+            Yellow_Fruit = 10,
+            Blue_Fruit = 5,
+            Red_Fruit = 5,
+            Pink_Fruit = 5,
+            Green_Fruit = 5,
+            Rainbow_Fruit = 1,
+        }
+
+        Captain = {
+            takedown = 7500,
+            Smile_Fruit = 12,
+            Yellow_Fruit = 3,
+            Blue_Fruit = 1,
+            Red_Fruit = 1,
+            Green_Fruit = 2,
+            Rainbow_Fruit = 1,
+        }
+
+        GhostKun = {
+            takedown = 5000,
+            Yellow_Fruit = 30,
+            Blue_Fruit = 10,
+            Red_Fruit = 13,
+            Pink_Fruit = 10,
+            Green_Fruit = 12,
+            Rainbow_Fruit = 12,
+        }
+
+        Shinobi = {
+            Water_Scroll = 50,
+            Fire_Scroll = 50,
+            Wood_Scroll = 50,
+            Yellow_Fruit = 12,
+            Blue_Fruit = 4,
+            Pink_Fruit = 3,
+            Green_Fruit = 3,
+            Rainbow_Fruit = 1,
+        }
+
+    }
+
+    return UnitsRequirement
